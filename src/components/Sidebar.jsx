fafdc678d203837f923b7c770b64e5bda249815e
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2024-11-30 13:59:01 +0500

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,9 +24,23 @@ export default function Sidebar() {
           </div>
 
           <div className="flex items-center gap-3">
-            <img src={assets.stack_icon} className="w-8" />
-            <p className="font-semibold">Your Library</p>
+            <img src={assets.arrow_icon} className="w-5" />
+            <img src={assets.plus_icon} className="w-5" />
           </div>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+          <h1>Create your first playlist</h1>
+          <p className="font-light">it's easy we will help you</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Create Playlist
+          </button>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
+          <h1>Let's findsome podcasts to follow</h1>
+          <p className="font-light">we'll keep you update on new episodes</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Browse podcasts
+          </button>
         </div>
       </div>
     </div>
