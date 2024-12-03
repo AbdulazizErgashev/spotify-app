ddeda7a12f6c6ac46c9c8b8cdf529ff61fec4d06
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2024-12-03 13:44:01 +0500

    updated codes, play and pause final

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 export default function Player() {
+  const { seekBg, seekBar, playStatus, play, pause } =
+    useContext(PlayerContext);
+
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
@@ -15,14 +19,33 @@ export default function Player() {
         <div className="flex gap-4">
           <img src={assets.shuffle_icon} className="w-4 cursor-pointer" />
           <img src={assets.prev_icon} className="w-4 cursor-pointer" />
-          <img src={assets.play_icon} className="w-4 cursor-pointer" />
+          {playStatus ? (
+            <img
+              onClick={pause}
+              src={assets.pause_icon}
+              className="w-4 cursor-pointer"
+            />
+          ) : (
+            <img
+              onClick={play}
+              src={assets.play_icon}
+              className="w-4 cursor-pointer"
+            />
+          )}
+
           <img src={assets.next_icon} className="w-4 cursor-pointer" />
           <img src={assets.loop_icon} className="w-4 cursor-pointer" />
         </div>
         <div className="flex items-center gap-5">
           <p>1:06</p>
-          <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-            <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
+          <div
+            ref={seekBg}
+            className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
+          >
+            <hr
+              ref={seekBar}
+              className="h-1 border-none w-0 bg-green-800 rounded-full"
+            />
           </div>
           <p>3:20</p>
         </div>
@@ -40,5 +63,5 @@ export default function Player() {
     </div>
   );
 }
-// 40:00
+// 01:52:30
 // https://www.youtube.com/watch?v=amFYvQK4huo&t=1442s
