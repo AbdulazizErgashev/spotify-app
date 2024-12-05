eeae3db52927ca4f1e7a6287f1c4fdd46ad1dc54
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2024-12-05 13:26:55 +0500

    minute/second

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from "react";
-import { assets, songsData } from "../assets/assets";
+import { assets } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 export default function Player() {
-  const { seekBg, seekBar, playStatus, play, pause } =
+  const { track, seekBg, seekBar, playStatus, play, pause, time } =
     useContext(PlayerContext);
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
-        <img className="w-12" src={songsData[0].image} />
+        <img className="w-12" src={track.image} />
         <div>
-          <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0, 12)}</p>
+          <p>{track.name}</p>
+          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
       <div className="flex flex-col items-center gap-1 m-auto">
@@ -37,7 +37,9 @@ export default function Player() {
           <img src={assets.loop_icon} className="w-4 cursor-pointer" />
         </div>
         <div className="flex items-center gap-5">
-          <p>1:06</p>
+          <p>
+            {time.currentTime.minute}:{time.currentTime.second}
+          </p>
           <div
             ref={seekBg}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
@@ -47,7 +49,9 @@ export default function Player() {
               className="h-1 border-none w-0 bg-green-800 rounded-full"
             />
           </div>
-          <p>3:20</p>
+          <p>
+            {time.totalTime.minute}:{time.totalTime.second}
+          </p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
@@ -63,6 +67,6 @@ export default function Player() {
     </div>
   );
 }
-// 01:52:30
+// 2:05:45
 // https://www.youtube.com/watch?v=amFYvQK4huo&t=1442s
-// yangi kodlar
+// SongsItem.jsx
